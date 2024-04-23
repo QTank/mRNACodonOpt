e9249f6445916b349f261205fab8c65bcda77ba2
zhanghongfeng <youdianre123@gmail.com>
2024-04-23 14:28:36 +0200

    delete test file

--- Test/TestAnsatz.py
+++
@@ -1,6 +0,0 @@
-from qiskit.circuit.library import EfficientSU2
-
-ansatz = EfficientSU2(17, entanglement='linear', reps=3)
-
-print(ansatz.decompose().num_nonlocal_gates())
-print(ansatz.decompose().depth())
